33be64bc73d8be75138b35f7f028f8cb36688df5
VoogdS <127872003+VoogdS@users.noreply.github.com>
2023-03-14 17:39:11 +0100

    Setup for testing document load performance

--- src/App.js
+++ src/App.js
@@ -1,43 +1,88 @@
-import React, { useRef, useEffect } from 'react';
-import WebViewer from '@pdftron/webviewer';
+import React, {useRef, useEffect, useState} from 'react';
+import WebViewerV6 from 'pdftronV6';
+import WebViewerV8 from 'pdftronV8';
 import './App.css';
 
 const App = () => {
-  const viewer = useRef(null);
+  const [v6LoadTime, setV6LoadTime] = useState();
+  const [v8LoadTime, setV8LoadTime] = useState();
 
-  // if using a class, equivalent of componentDidMount 
+  const viewerV6 = useRef(null);
+  const viewerV8 = useRef(null);
+
+  const pdfUrl = 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf';
+
+  let v6LoadStart;
+  let v6LoadEnd;
+
+  let v8LoadStart;
+  let v8LoadEnd;
+
+  const x = 6;
+
   useEffect(() => {
-    WebViewer(
+    WebViewerV6(
       {
-        path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        path: '/webviewer/lib/v6',
       },
-      viewer.current,
+      viewerV6.current,
     ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+      const { docViewer } = instance;
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
+      docViewer.on('documentLoaded', () => {
+        // We stop the timer when the document viewer tells us that the document has been loaded
+        v6LoadEnd = performance.now();
+        const loadTime = v6LoadEnd - v6LoadStart;
+        console.log(`v6.2.1 document loaded: ${loadTime}ms`)
+        setV6LoadTime(loadTime)
 
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
+        // We've loaded the document using v6.2.1, now do the same using v8.12.0
+        loadV8();
       });
+
+      // We start the timer when we tell the document viewer to load the PDF from the URL
+      v6LoadStart = performance.now();
+      docViewer.loadDocument(pdfUrl);
     });
   }, []);
 
+  const loadV8 = () => {
+    WebViewerV8(
+      {
+        path: '/webviewer/lib/v8',
+      },
+      viewerV8.current,
+    ).then((instance) => {
+      const { documentViewer } = instance.Core;
+
+      documentViewer.addEventListener('documentLoaded', () => {
+        v8LoadEnd = performance.now();
+        const loadTime = v8LoadEnd - v8LoadStart;
+        console.log(`v8.12.0 document loaded: ${loadTime}ms`)
+        setV8LoadTime(loadTime);
+      });
+
+      v8LoadStart = performance.now();
+      documentViewer.loadDocument(pdfUrl);
+    });
+  }
+
   return (
     <div className="App">
-      <div className="header">React sample</div>
-      <div className="webviewer" ref={viewer}></div>
+      <div className="version v6">
+        <div className="header">
+          <div className="title">v6.2.1</div>
+          <div className="time">{v6LoadTime ? v6LoadTime + 'ms' : ''}</div>
+        </div>
+        <div className="webviewer" ref={viewerV6}></div>
+      </div>
+      <div className="version v8">
+        <div className="header">
+          <div className="title">v8.12.0</div>
+          <div className="time">{v8LoadTime ? v8LoadTime + 'ms' : ''}</div>
+        </div>
+        <div className="webviewer" ref={viewerV8}></div>
+      </div>
     </div>
   );
 };
--- tools/copy-webviewer-files.js
+++ tools/copy-webviewer-files.js
@@ -2,7 +2,8 @@ const fs = require('fs-extra');
 
 const copyFiles = async () => {
   try {
-    await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer/lib');
+    await fs.copy('./node_modules/pdftronV6/public', './public/webviewer/lib/v6');
+    await fs.copy('./node_modules/pdftronV8/public', './public/webviewer/lib/v8');
     console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
